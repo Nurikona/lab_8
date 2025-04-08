ea5f83a3eaae43a746bf86dcfeb343dc74511296
Nur <assetnut2005@gmail.com>
2025-04-08 12:30:27 +0500

    10

---
+++ app/src/main/java/com/example/lab_8/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.lab_8
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var reminderIntent: Intent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        reminderIntent = Intent(this, ReminderService::class.java)
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_start_reminder -> startService(reminderIntent)
+            R.id.button_stop_reminder -> stopService(reminderIntent)
+            R.id.button_next_activity -> {
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
